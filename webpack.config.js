021da2bc6749a901ab9bfc1ca82e40bbf26e27a0
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-10-08 10:43:36 -0700

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,6 @@ module.exports = {
 					options: {
 						plugins: function () {
 							return [ 
-                require('precss'),
                 require('autoprefixer')
               ]
 						}
