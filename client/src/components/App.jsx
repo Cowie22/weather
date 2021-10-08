5c125f61431dfd14b78eff5d54da9c0f3b676974
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-10-08 10:48:29 -0700

    Update

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,29 +6,14 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { AppContext } from '../contexts/state'
 
 const App = (props) => {
-  const state = useContext(AppContext);
-  const {
-    isCookieVisible,
-    handleIsCookieVisible
-  } = state;
   return (
     <section className='app-container'>
       <Container>
         <Row>
           <Col lg={{span: 10, offset: 1}}>
-            <h1 className='gold' onClick={() => handleIsCookieVisible(!isCookieVisible)}>
+            <h1 className='gold'>
               HELLO WORLD
             </h1>
-            {
-              isCookieVisible ?
-                <h2 className='gold'>
-                  HELLO HIDDEN WORLD
-                </h2>
-              :
-                <div>
-
-                </div>
-            }
           </Col>
         </Row>
       </Container>
